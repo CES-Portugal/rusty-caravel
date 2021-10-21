b126cea45aa0404d4e4c69c3251cf4389e15d6dc
Francisco Prospero <xico_prospero@hotmail.com>
2021-10-21 23:41:05 +0100

    Send function is async, spawns new thread, and uses regex to check flags

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io::{self, BufRead};
 use tokio::signal;
 use tokio::sync::watch;
-
+use tokio::time::{sleep, Duration};
+use regex::Regex;
 
 async fn say_world() {
     println!("world");
@@ -13,38 +14,55 @@ async fn ctrlc_handler(tx: watch::Sender<&str>) {
     tx.send("world").expect("TODO");
 }
 //clap rust -> cmd
+
 async fn read_input(mut rx: watch::Receiver<&str>) {
     let mut line = String::new();
     let stdin = io::stdin();
+    let re_send = Regex::new("^send").unwrap();
+    //let handle_send = tokio::spawn(async move {send_can()}.await );
 
     loop {
+
         // if rx.changed().await.is_ok() {
         //     println!("Stop processing user input");
         //     break;
         // }
         stdin.lock().read_line(&mut line).expect("Could not read line");
-
         let op = line.trim_right();
+        
         if op == "EXIT" {
             break;
-        } else if op == "send" {
-            send_can();
+        } else if re_send.is_match(op) {
+            tokio::spawn(async move {send_can(op.to_string())}.await).await;
         }
         line.clear()
     }
 }
 
-fn send_can() {
+async fn send_can(cmd: String) {
+    let re_canid = Regex::new(r".* -id (\d+)").unwrap();
+    let re_candata = Regex::new(r".* -m [[:punct:]](.*)[[:punct:]]").unwrap();
     let mut canid = String::new();
     let mut msg = String::new();
     let mut stdin = io::stdin();
-
-    println!("CAN ID:");
-    stdin.lock().read_line(&mut canid).expect("Could not read line");
-    println!("Message:");
-    stdin.lock().read_line(&mut msg).expect("Could not read line");
-    send(canid, msg);
+    // println!("match, {}", &cmd);
+    if re_canid.is_match(&cmd) {
+        let cap = re_canid.captures(&cmd).unwrap();
+        canid = cap[1].to_string();
+        println!("{} -> CAN id", canid);
+    }
+    if re_candata.is_match(&cmd) {
+        let cap = re_candata.captures(&cmd).unwrap();
+        msg = cap[1].to_string();
+        println!("{} -> CAN data", msg);
+    }
+    // println!("CAN ID:");
+    // stdin.lock().read_line(&mut canid).expect("Could not read line");
+    // println!("Message:");
+    // stdin.lock().read_line(&mut msg).expect("Could not read line");
+    // send(canid, msg);
 }
+
 
 fn send(canid : String, msg : String) {
     println!("Message Sent \nid: {}message: {}", canid, msg);
@@ -67,5 +85,6 @@ async fn main() {
     op.await;
     handle_input.await;
     handle_ctrl_c.await;
+
 
 }
